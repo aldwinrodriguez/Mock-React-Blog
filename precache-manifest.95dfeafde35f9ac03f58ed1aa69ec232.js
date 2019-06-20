43688a0f5a068e40df72a904ea9448887150266f
Aldwin Rodriguez <aldwin.rodriguez@owl.ucc.edu>
2019-06-20 12:53:33 -0400

    Updates

---
+++ precache-manifest.95dfeafde35f9ac03f58ed1aa69ec232.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b56c31e8958f4953743aeb66bbe81c",
+    "url": "/Mock-React-Blog/index.html"
+  },
+  {
+    "revision": "0f915ef66d9e2036e4e6",
+    "url": "/Mock-React-Blog/static/css/main.3af63a28.chunk.css"
+  },
+  {
+    "revision": "d90020a00927438ba58c",
+    "url": "/Mock-React-Blog/static/js/2.19406b62.chunk.js"
+  },
+  {
+    "revision": "0f915ef66d9e2036e4e6",
+    "url": "/Mock-React-Blog/static/js/main.2911d193.chunk.js"
+  },
+  {
+    "revision": "2f7eefd8b592c338320c",
+    "url": "/Mock-React-Blog/static/js/runtime~main.74653f90.js"
+  }
+]);
